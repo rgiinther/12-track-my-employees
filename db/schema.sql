7418780b73316c244b32ce0640b9364bac3e7401
rgiinther <r.giin95@gmail.com>
2022-07-21 00:38:28 -0500

    finished database

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,26 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INTEGER,
+    role_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,30 @@ VALUES
 ('Sales'),
 ('Legal'),
 ('Human Resources');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Senior Web Developer', 130000, 1),
+('Web Developer', 90000, 1), 
+('Lead Office Manager', 135000, 2),
+('Accountant', 60000, 2),
+('Sales', 65000, 3),
+('Legal', 120000, 4),
+('Human Resources Manager', 70000, 5),
+('Human Resources', 40000, 5);
+
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
+VALUES
+('Ryu', 'Hayabusa', null, 1),
+('Leon', 'Kennedy', 1, 2),
+('Jill', 'Valentine', 1, 2),
+('Marcus', 'Fenix', null, 3),
+('Rean', 'Schwarzer', 4, 4),
+('Laura', 'Arseid', 4, 4),
+('Giliath', 'Osborne', 4, 5),
+('Olivert', 'Arnor', 4, 5),
+('Rufus', 'Alberea', 4, 6),
+('Fie', 'Claussel', 4, 6),
+('Sara', 'Velestein', null, 7),
+('Towa', 'Herschel', 11, 8),
+('Emma', 'Millstein', 11, 8);
